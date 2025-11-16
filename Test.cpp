254f948da7c1c7735102e1208ee4bd00ae134154
meganate100 <meganate100@gmail.com>
2025-11-16 13:40:18 -0800

    Update Test.cpp
    This should actually be the updated code now

--- Test.cpp
+++ Test.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <cmath>
 //TestBestTheRest
 float DotProduct(float x1, float y1, float z1, float x2, float y2, float z2){//Function that will take a string which describes the particle type (eg: "O" for oxygen, "H" for Hydrogen) and translate it to the appropriate mass
 float result = ((x1*x2) + (y1*y2) + (z1*z2));
@@ -29,12 +30,10 @@ struct Particle {
     float H1xCoM, H1yCoM, H1zCoM;
     float H2xCoM, H2yCoM, H2zCoM;
     float VxCoM, VyCoM, VzCoM;
-    float xDip;
-    float yDip;
-    float zDip;
-    float IDK;
+    float xDip, yDip, zDip;
     float timestamp;
-    float Mass;
+    int j = -1;
+    int k = 0;
 
 while(std::getline(file, line)){
     streamMcgee.seekg(0, std::stringstream::beg);
@@ -42,20 +41,21 @@ while(std::getline(file, line)){
     streamMcgee >> DontCare;
 
     if(DontCare == "Generated"){//This checks to see if the next line is starting a new timestamp.
+        j++;
         streamMcgee.seekg(31, std::stringstream::beg);//Skips to where the timestamp will be in the line
         streamMcgee >> timestamp;
-        TestOutput << timestamp << std::endl;     
+        TestOutput << std::endl << timestamp << std::endl;     
         std::getline(file, line);
         streamMcgee.str(line);
         streamMcgee.seekg(0, std::stringstream::beg);
         streamMcgee >> PartNum;
         TestOutput << PartNum << std::endl;
 
-        for (int i = 1; i < 3; i++) {
+        for (int i = 1; i < (PartNum/4) +1; i++) {
             TestOutput << std::endl << i;
             std::getline(file, line);//Get Info on the Oxygen
             streamMcgee.str(line);
-            TestOutput << std::endl << line << std::endl;
+            TestOutput << line << std::endl;
             streamMcgee.seekg(20, std::stringstream::beg);
             streamMcgee >> xCoM >> yCoM >> zCoM;
 
@@ -63,16 +63,19 @@ while(std::getline(file, line)){
 
             std::getline(file, line);//Get Info on the 1st Hydrogen
             streamMcgee.str(line);
+            TestOutput << line << std::endl;
             streamMcgee.seekg(20, std::stringstream::beg);
             streamMcgee >> H1xCoM >> H1yCoM >> H1zCoM;
 
             std::getline(file, line);//Get Info on the 2nd Hydrogen
             streamMcgee.str(line);
+            TestOutput << line << std::endl;
             streamMcgee.seekg(20, std::stringstream::beg);
             streamMcgee >> H2xCoM >> H2yCoM >> H2zCoM;
 
             std::getline(file, line);//Get Info on the virtual site
             streamMcgee.str(line);
+            TestOutput << line << std::endl;
             streamMcgee.seekg(20, std::stringstream::beg);
             streamMcgee >> VxCoM >> VyCoM >> VzCoM;
 
@@ -80,15 +83,17 @@ while(std::getline(file, line)){
             yDip = (Charge)*(H1yCoM + H2yCoM- 2*VyCoM);
             zDip = (Charge)*(H1zCoM + H2zCoM- 2*VzCoM);
 
-            TestOutput << "   " << xCoM << "   " << yCoM << "   " << zCoM << std::endl;
             Part.push_back({timestamp, xCoM, yCoM, zCoM, CoMDist, xDip, yDip, zDip});//Input all relevant info into a particle structure
-            TestOutput << "Timestamp: " << Part.at(i-1).timestamp << "  xCoM: " << Part.at(i-1).xCoM 
-            << "  yCoM: " << Part.at(i-1).yCoM << "  zCoM: " << Part.at(i-1).zCoM << "  Radial Distance: " << Part.at(i-1).CoMDist 
-            << "  Net xDipole: " << Part.at(i-1).xDip << "  Net yDipole: " << Part.at(i-1).yDip << "  Net zDipole: " << Part.at(i-1).zDip << std::endl;
+            TestOutput << "Timestamp: " << Part.at(((j)*(PartNum/4))+i-1).timestamp << "  xCoM: " << Part.at(((j)*(PartNum/4) )+i-1).xCoM 
+            << "  yCoM: " << Part.at(((j)*(PartNum/4) )+i-1).yCoM << "  zCoM: " << Part.at(((j)*(PartNum/4) )+i-1).zCoM 
+            << "  Radial Distance: " << Part.at(((j)*(PartNum/4) )+i-1).CoMDist << "  Net xDipole: " 
+            << Part.at(((j)*(PartNum/4) )+i-1).xDip << "  Net yDipole: " << Part.at(((j)*(PartNum/4) )+i-1).yDip 
+            << "  Net zDipole: " << Part.at(((j)*(PartNum/4) )+i-1).zDip << std::endl;
         }
         
     } else{
-        std::cout << "The else has been activated" << std::endl;
+        k++;
+        std::cout << "The else has been activated " << k <<std::endl;
     }
 }
 TestOutput << "The Program might have worked" << std::endl;
